9a26f2d17f9060289540c3df738d62f3c99ba9d3
Mueid <mrmuyed579@gmail.com>
2022-05-22 20:33:20 +0600

    data send to client

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
 
+
+
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.5ftjy.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const productCollection = client.db('pcworldsdb').collection('products');
+
+
+        app.get('/product', async(req,res)=>{
+            const query= {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
+
+
 app.get('/', (req,res)=>{
-    req.send('Hello from server');
+    res.send('Hello from server');
 });
 
 app.listen(port, ()=>{
